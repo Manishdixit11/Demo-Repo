651d9e33ca3a1461fade9f557b79a127cdee65c3
Manishdixit11 <167887931+Manishdixit11@users.noreply.github.com>
2025-01-16 20:08:17 +0530

    Create index.jsx

---
+++ index.jsx
@@ -0,0 +1,14 @@
+// src/HomePage.js
+import React from 'react';
+
+const HomePage = () => {
+    return (
+        <div style={{ textAlign: 'center', padding: '50px' }}>
+            <h1>Welcome to My Home Page!</h1>
+            <p>This is a simple home page built with React.</p>
+        </div>
+    );
+};
+
+export default HomePage;
+
